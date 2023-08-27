0907f6a46a8e9b4bc39cdc2fa16a99303c494489
unknown <jacobmarcosoliver@gmail.com>
2023-08-27 07:25:40 +0330

    test after

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,38 +7,46 @@ import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import Register from './components/register';
 import Search from './components/search';
 import IRANSansWeb from './../../assets/fonts/IRANSansWeb.woff2';
+import sepahImage from './../../assets/sepah.png';
 
 function Main() {
   return (
-    <div className="flex flex-col p-4 h-screen justify-between bg-yellow-300">
-      <div className="flex justify-center rounded-2xl">
-        <img
-          src="https://farsgraphic.com/wp-content/uploads/2021/10/دانلود-رایگان-وکتور-لوگو-آرم-سپاه-پاسداران.png"
-          alt=""
-          className="max-h-fit"
-        />
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-center font-bold text-4xl bg-green-900 text-yellow-500 p-8 rounded-xl">
-          پایگاه مقاومت بسیج شهید مهدی نریمی
-        </div>
-        <div className=" flex gap-2">
-          <div className="w-1/2 bg-amber-700 rounded-2xl">
-            <Link
-              to="/search"
-              className="w-full text-white flex justify-center p-4 items-center hover:bg-slate-200 font-bold text-2xl"
-            >
-              جستجو در اطلاعات ثبت شده
-            </Link>
+    <div className="h-screen bg-yellow-300">
+      <div className="h-fit bg-yellow-300">
+        <div className="flex flex-col p-4  justify-between h-screen">
+          <div
+            style={{ height: 650 }}
+            className="flex justify-center rounded-2xl w-auto"
+          >
+            <img
+              src={sepahImage}
+              alt=""
+              className="h-full max-w-full object-contain"
+            />
           </div>
-          <div className="w-1/2  bg-blue-500 rounded-2xl">
-            <Link
-              to="/register"
-              className="w-full text-white flex justify-center p-4 items-center hover:bg-slate-200 font-bold text-2xl"
-            >
-              ثبت اطلاعات جدید
-            </Link>
+
+          <div className="flex flex-col gap-2  rounded-2xl">
+            <div className="flex justify-center font-bold text-4xl bg-green-900 text-yellow-500 p-8 rounded-xl">
+              پایگاه مقاومت بسیج شهید مهدی نریمی
+            </div>
+            <div className=" flex gap-2">
+              <div className="w-1/2 bg-amber-700 rounded-2xl">
+                <Link
+                  to="/search"
+                  className="w-full text-white flex justify-center p-4 items-center hover:bg-slate-200 font-bold text-2xl"
+                >
+                  جستجو در اطلاعات ثبت شده
+                </Link>
+              </div>
+              <div className="w-1/2  bg-blue-500 rounded-2xl">
+                <Link
+                  to="/register"
+                  className="w-full text-white flex justify-center p-4 items-center hover:bg-slate-200 font-bold text-2xl"
+                >
+                  ثبت اطلاعات جدید
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/renderer/components/register.tsx
+++ src/renderer/components/register.tsx
@@ -85,87 +85,89 @@ export default function Register() {
   }
 
   return (
-    <div className="flex p-4 gap-4 justify-between min-w-fit resize-none">
-      <Link
-        to="/"
-        className="bg-yellow-400 text-2xl p-2 rounded-xl h-fit min-w-fit"
-      >
-        بازگشت
-      </Link>
+    <div className="">
+      <div className="flex p-4 gap-4 justify-between min-w-fit">
+        <Link
+          to="/"
+          className="bg-yellow-400 text-2xl p-2 rounded-xl h-fit min-w-fit border-black border-4"
+        >
+          بازگشت
+        </Link>
 
-      <form
-        action="/"
-        method="post"
-        className="flex flex-col gap-4 bg-emerald-800 p-4 rounded-xl w-11/12 min-w-fit"
-      >
-        <input
-          type="text"
-          className="border-4 outline-none placeholder:p-2 rounded-xl h-12"
-          dir="rtl"
-          placeholder="نام و نام خانوادگی"
-          onChange={handleChangeFullName}
-        />
-        <input
-          type="text"
-          className="border-4 outline-none placeholder:p-2 rounded-xl h-12"
-          dir="rtl"
-          placeholder="کد ملی"
-          onChange={handleChangeNationalCode}
-        />
-        <input
-          type="text"
-          className="border-4 outline-none placeholder:p-2 rounded-xl h-12"
-          dir="rtl"
-          placeholder="شماره همراه"
-          onChange={handleChangeMobile}
-        />
-        <div className="flex gap-2 justify-between">
+        <form
+          action="/"
+          method="post"
+          className="flex flex-col gap-4 bg-emerald-800 p-4 rounded-xl"
+        >
           <input
             type="text"
-            className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+            className="border-4 outline-none placeholder:p-2 rounded-xl h-12"
             dir="rtl"
-            placeholder="سال"
-            onChange={handleChangeYear}
+            placeholder="نام و نام خانوادگی"
+            onChange={handleChangeFullName}
           />
           <input
             type="text"
-            className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+            className="border-4 outline-none placeholder:p-2 rounded-xl h-12"
             dir="rtl"
-            placeholder="ماه"
-            onChange={handleChangeMonth}
+            placeholder="کد ملی"
+            onChange={handleChangeNationalCode}
           />
           <input
             type="text"
-            className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+            className="border-4 outline-none placeholder:p-2 rounded-xl h-12"
             dir="rtl"
-            placeholder="روز"
-            onChange={handleChangeDay}
+            placeholder="شماره همراه"
+            onChange={handleChangeMobile}
           />
-          <div className="flex items-center border-2 px-2 bg-lime-200 h-12">
-            تاریخ تولد
+          <div className="flex gap-2 justify-between">
+            <input
+              type="text"
+              className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+              dir="rtl"
+              placeholder="سال"
+              onChange={handleChangeYear}
+            />
+            <input
+              type="text"
+              className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+              dir="rtl"
+              placeholder="ماه"
+              onChange={handleChangeMonth}
+            />
+            <input
+              type="text"
+              className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+              dir="rtl"
+              placeholder="روز"
+              onChange={handleChangeDay}
+            />
+            <div className="flex items-center border-2 px-2 bg-lime-200 h-12">
+              تاریخ تولد
+            </div>
           </div>
-        </div>
-        <input
-          type="text"
-          className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
-          dir="rtl"
-          placeholder="آدرس"
-          onChange={handleChangeAddress}
-        />
-        <button
-          id="upload-tasvir"
-          onClick={selectImages}
-          className="bg-white border-4 outline-none placeholder:p-2 h-12 rounded-xl"
-        >
-          انتخاب تصویر
-        </button>
-        <button
-          className="bg-yellow-200 rounded-xl h-12"
-          onClick={registerUserInfo}
-        >
-          ثبت
-        </button>
-      </form>
+          <input
+            type="text"
+            className="border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+            dir="rtl"
+            placeholder="آدرس"
+            onChange={handleChangeAddress}
+          />
+          <button
+            id="upload-tasvir"
+            onClick={selectImages}
+            className="bg-white border-4 outline-none placeholder:p-2 h-12 rounded-xl"
+          >
+            انتخاب تصویر
+          </button>
+          <button
+            className="bg-yellow-200 rounded-xl h-12"
+            onClick={registerUserInfo}
+          >
+            ثبت
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
