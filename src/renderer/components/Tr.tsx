afdc7449926292cfb87ef0002b969187e5fa5ce0
unknown <jacobmarcosoliver@gmail.com>
2023-09-09 17:50:23 +0330

    some undifined functions removed.

--- src/renderer/components/Tr.tsx
+++ src/renderer/components/Tr.tsx
@@ -28,14 +28,16 @@ export default function Tr(props) {
     let birthDate = TrRef.current.childNodes[2].textContent;
     let address = TrRef.current.childNodes[3].textContent;
     let mobile = TrRef.current.childNodes[4].textContent;
-    window.electron.ipcRenderer.invokeUpdateUser({
+    let newInfo = {
       user_id: user_id,
       fullName: fullName,
       nationalCode: nationalCode,
       birthDate: birthDate,
       address: address,
       mobile: mobile,
-    });
+    };
+    // window.electron.ipcRenderer.invokeUpdateUser(newInfo);
+    console.log(newInfo);
     // alert.show('تغییرات با موفقیت دخیره شد', {
     //   timeout: 1500, // custom timeout just for this one alert
     //   type: 'success',
@@ -55,39 +57,19 @@ export default function Tr(props) {
       className="odd:bg-gray-100"
       ref={TrRef}
     >
-      <td
-        className="border-4 p-2 outline-none"
-        contentEditable
-        onInput={handleChangeFullname}
-      >
+      <td className="border-4 p-2 outline-none" contentEditable>
         {props.user.full_name}
       </td>
-      <td
-        className="border-4 p-2 outline-none"
-        contentEditable
-        onInput={handleChangeNationalcode}
-      >
+      <td className="border-4 p-2 outline-none" contentEditable>
         {props.user.national_code}
       </td>
-      <td
-        className="border-4 p-2 outline-none"
-        contentEditable
-        onInput={handleChangeBirthDate}
-      >
+      <td className="border-4 p-2 outline-none" contentEditable>
         {props.user.birth_date}
       </td>
-      <td
-        className="border-4 p-2 outline-none"
-        contentEditable
-        onInput={handleChangeAddress}
-      >
+      <td className="border-4 p-2 outline-none" contentEditable>
         {props.user.address}
       </td>
-      <td
-        className="border-4 p-2 outline-none"
-        contentEditable
-        onInput={handleChangeMobile}
-      >
+      <td className="border-4 p-2 outline-none" contentEditable>
         {props.user.mobile}
       </td>
       <td className="bg-orange-400 p-2 border-4 hover:bg-orange-600">
